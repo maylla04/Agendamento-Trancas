81975d56ccd570924b37c5392713180eb172c92c
Maylla Nascimento <maylla.rie@gmail.com>
2026-06-22 09:04:22 -0300

    trocando useEffect ou UseFocusEffect, inserindo css na home

--- agendamento/src/screens/HomeScreen.tsx
+++ agendamento/src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../App';
 import { Agendamento } from '../types/agendamento';
@@ -14,26 +15,51 @@ export default function HomeScreen({ navigation }: Props) {
   const db = useSQLiteContext()
   const [agendamentos, setAgendamentos] = useState<Agendamento[]>([]);
     
-  useEffect(() => {
-    carregarAgendamentos()
-}, []);
+  useFocusEffect(
+    useCallback(() => {
+        carregarAgendamentos();
+    }, [])
+);
 
-async function carregarAgendamentos() {
+  async function carregarAgendamentos() {
     const resultado = await listarAgendamentos(db);
     setAgendamentos(resultado)
-}
+  }
+
   return (
-    <View>
-      <Text>Agendamento de tranças - Maylla Braids</Text>
-      <BotaoPrimario texto="Novo Agendamento" onPress={() => navigation.navigate('NovoAgendamento')}/>
-      
+    <View style={styles.container}>
+      <Text style={styles.titulo}>✂️ Maylla Braids</Text>
+      <Text style={styles.subtitulo}>Agendamentos</Text>
+      <BotaoPrimario texto="+ Novo Agendamento" onPress={() => navigation.navigate('NovoAgendamento')}/>
       <FlatList
-    data={agendamentos}
-    keyExtractor={(item) => String(item.id)}
-    renderItem={({ item }) => (
-        <CardAgendamento agendamento={item} />
-    )}
-/>
+        data={agendamentos}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate('Detalhes', { agendamento: item })}>
+            <CardAgendamento agendamento={item} />
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5ECD7',
+    padding: 20,
+    paddingTop: 50,
+  },
+  titulo: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#6B3F2A',
+    marginBottom: 4,
+  },
+  subtitulo: {
+    fontSize: 16,
+    color: '#4A2C1A',
+    marginBottom: 20,
+  },
+});
